2507eb3b7ed739c06e99bea6f251705d217ee253
Brain Aboze <abozejohn@gmail.com>
2021-07-29 02:06:40 +0100

    Completed frontend of online prediction

--- app.py
+++ app.py
@@ -8,3 +8,89 @@ from PIL import Image
 import joblib
 model = joblib.load(r"./notebook/model.sav")
 
+def main():
+    #Setting Application title
+    st.title('Telco Customer Churn Prediction App')
+
+      #Setting Application description
+    st.markdown("""
+     :dart:  This Streamlit app is made to predict customer churn in a ficitional telecommunication use case.
+    The application is functional for both online prediction and batch data prediction. \n
+    """)
+    st.markdown("<h3></h3>", unsafe_allow_html=True)
+
+    #Setting Application sidebar default
+    image = Image.open('App.jpg')
+    add_selectbox = st.sidebar.selectbox(
+	"How would you like to predict?", ("Online", "Batch"))
+    st.sidebar.info('This app is created to predict Customer Churn')
+    st.sidebar.image(image)
+
+    if add_selectbox == "Online":
+      st.info("Input data below")
+      #Based on our optimal features selection
+      st.subheader("Demographic data")
+      seniorcitizen = st.selectbox('Senior Citizen:', ['Yes', 'No'])
+      dependents = st.selectbox('Dependent:', ['Yes', 'No'])
+
+
+      st.subheader("Payment data")
+      tenure = st.slider('Number of months the customer has stayed with the company', min_value=0, max_value=72, value=0)
+      contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
+      paperlessbilling = st.selectbox('Paperless Billing', ['Yes', 'No'])
+      PaymentMethod = st.selectbox('PaymentMethod',['Electronic check', 'Mailed check', 'Bank transfer (automatic)','Credit card (automatic)'])
+      monthlycharges = st.number_input('The amount charged to the customer monthly', min_value=0, max_value=150, value=0)
+      totalcharges = st.number_input('The total amount charged to the customer',min_value=0, max_value=10000, value=0)
+
+      st.subheader("Services signed up for")
+      mutliplelines = st.selectbox("Does the customer have multiple lines",['Yes','No','No phone service'])
+      phoneservice = st.selectbox('Phone Service:', ['Yes', 'No'])
+      internetservice = st.selectbox("Does the customer have internet service", ['DSL', 'Fiber optic', 'No'])
+      onlinesecurity = st.selectbox("Does the customer have online security",['Yes','No','No internet service'])
+      onlinebackup = st.selectbox("Does the customer have online backup",['Yes','No','No internet service'])
+      techsupport = st.selectbox("Does the customer have technology support", ['Yes','No','No internet service'])
+      streamingtv = st.selectbox("Does the customer stream TV", ['Yes','No','No internet service'])
+      streamingmovies = st.selectbox("Does the customer stream movies", ['Yes','No','No internet service'])
+      
+      data = {
+                'SeniorCitizen': [seniorcitizen],
+                'Dependents': [dependents],
+                'tenure':[tenure],
+                'PhoneSevice': [phoneservice],
+                'MultipleLines': [mutliplelines],
+                'InternetService': [internetservice],
+                'OnlineSecurity': [onlinesecurity],
+                'OnlineBackup': [onlinebackup],
+                'TechSupport': [techsupport],
+                'StreamingTV': [streamingtv],
+                'StreamingMovies': [streamingmovies],
+                'Contract': [contract],
+                'PaperlessBilling': [paperlessbilling],
+                'PaymentMethod':[PaymentMethod], 
+                'MonthlyCharges':[monthlycharges], 
+                'TotalCharges': [totalcharges]
+                }
+    features_df = pd.DataFrame(data)
+    st.markdown("<h3></h3>", unsafe_allow_html=True)
+    st.write('Overview of input is shown below')
+    st.markdown("<h3></h3>", unsafe_allow_html=True)
+    st.dataframe(features_df)
+
+
+    
+     
+        
+
+
+
+    pass
+
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
