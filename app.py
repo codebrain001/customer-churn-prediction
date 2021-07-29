0391313eb6447ed139f9c17e4d8aef0d32845cbf
Brain Aboze <abozejohn@gmail.com>
2021-07-29 03:24:52 +0100

    Completed preprocessing script

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from PIL import Image
 import joblib
 model = joblib.load(r"./notebook/model.sav")
 
+#Import python scripts
+from preprocessing import preprocess
+
 def main():
     #Setting Application title
     st.title('Telco Customer Churn Prediction App')
@@ -30,54 +33,56 @@ def main():
       st.info("Input data below")
       #Based on our optimal features selection
       st.subheader("Demographic data")
-      seniorcitizen = st.selectbox('Senior Citizen:', ['Yes', 'No'])
-      dependents = st.selectbox('Dependent:', ['Yes', 'No'])
+      seniorcitizen = st.selectbox('Senior Citizen:', ('Yes', 'No'))
+      dependents = st.selectbox('Dependent:', ('Yes', 'No'))
 
 
       st.subheader("Payment data")
       tenure = st.slider('Number of months the customer has stayed with the company', min_value=0, max_value=72, value=0)
-      contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
-      paperlessbilling = st.selectbox('Paperless Billing', ['Yes', 'No'])
-      PaymentMethod = st.selectbox('PaymentMethod',['Electronic check', 'Mailed check', 'Bank transfer (automatic)','Credit card (automatic)'])
+      contract = st.selectbox('Contract', ('Month-to-month', 'One year', 'Two year'))
+      paperlessbilling = st.selectbox('Paperless Billing', ('Yes', 'No'))
+      PaymentMethod = st.selectbox('PaymentMethod',('Electronic check', 'Mailed check', 'Bank transfer (automatic)','Credit card (automatic)'))
       monthlycharges = st.number_input('The amount charged to the customer monthly', min_value=0, max_value=150, value=0)
       totalcharges = st.number_input('The total amount charged to the customer',min_value=0, max_value=10000, value=0)
 
       st.subheader("Services signed up for")
-      mutliplelines = st.selectbox("Does the customer have multiple lines",['Yes','No','No phone service'])
-      phoneservice = st.selectbox('Phone Service:', ['Yes', 'No'])
-      internetservice = st.selectbox("Does the customer have internet service", ['DSL', 'Fiber optic', 'No'])
-      onlinesecurity = st.selectbox("Does the customer have online security",['Yes','No','No internet service'])
-      onlinebackup = st.selectbox("Does the customer have online backup",['Yes','No','No internet service'])
-      techsupport = st.selectbox("Does the customer have technology support", ['Yes','No','No internet service'])
-      streamingtv = st.selectbox("Does the customer stream TV", ['Yes','No','No internet service'])
-      streamingmovies = st.selectbox("Does the customer stream movies", ['Yes','No','No internet service'])
+      mutliplelines = st.selectbox("Does the customer have multiple lines",('Yes','No','No phone service'))
+      phoneservice = st.selectbox('Phone Service:', ('Yes', 'No'))
+      internetservice = st.selectbox("Does the customer have internet service", ('DSL', 'Fiber optic', 'No'))
+      onlinesecurity = st.selectbox("Does the customer have online security",('Yes','No','No internet service'))
+      onlinebackup = st.selectbox("Does the customer have online backup",('Yes','No','No internet service'))
+      techsupport = st.selectbox("Does the customer have technology support", ('Yes','No','No internet service'))
+      streamingtv = st.selectbox("Does the customer stream TV", ('Yes','No','No internet service'))
+      streamingmovies = st.selectbox("Does the customer stream movies", ('Yes','No','No internet service'))
       
       data = {
-                'SeniorCitizen': [seniorcitizen],
-                'Dependents': [dependents],
-                'tenure':[tenure],
-                'PhoneSevice': [phoneservice],
-                'MultipleLines': [mutliplelines],
-                'InternetService': [internetservice],
-                'OnlineSecurity': [onlinesecurity],
-                'OnlineBackup': [onlinebackup],
-                'TechSupport': [techsupport],
-                'StreamingTV': [streamingtv],
-                'StreamingMovies': [streamingmovies],
-                'Contract': [contract],
-                'PaperlessBilling': [paperlessbilling],
-                'PaymentMethod':[PaymentMethod], 
-                'MonthlyCharges':[monthlycharges], 
-                'TotalCharges': [totalcharges]
+                'SeniorCitizen': seniorcitizen,
+                'Dependents': dependents,
+                'tenure':tenure,
+                'PhoneService': phoneservice,
+                'MultipleLines': mutliplelines,
+                'InternetService': internetservice,
+                'OnlineSecurity': onlinesecurity,
+                'OnlineBackup': onlinebackup,
+                'TechSupport': techsupport,
+                'StreamingTV': streamingtv,
+                'StreamingMovies': streamingmovies,
+                'Contract': contract,
+                'PaperlessBilling': paperlessbilling,
+                'PaymentMethod':PaymentMethod, 
+                'MonthlyCharges': monthlycharges, 
+                'TotalCharges': totalcharges
                 }
-    features_df = pd.DataFrame(data)
+    features_df = pd.DataFrame.from_dict([data])
     st.markdown("<h3></h3>", unsafe_allow_html=True)
     st.write('Overview of input is shown below')
     st.markdown("<h3></h3>", unsafe_allow_html=True)
     st.dataframe(features_df)
 
 
-    
+    #Preprocess inputs
+    preprocess_df = preprocess(features_df, 'Online')
+    st.dataframe(preprocess_df)
      
         
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,43 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-def preprocessing(df):
+def preprocess(df, option):
     """
     This function is to cover all the preprocessing steps on the churn dataframe. It involves selecting important features, encoding categorical data, handling missing values,feature scaling and splitting the data
-    
     """
+    #Defining the map function
+    def binary_map(feature):
+        return feature.map({'Yes':1, 'No':0})
+
+    # Encode binary categorical features
+    binary_list = ['SeniorCitizen','Dependents', 'PhoneService', 'PaperlessBilling']
+    df[binary_list] = df[binary_list].apply(binary_map)
+
+    
+    #Drop values based on operational options
+    if (option == "Online"):
+        columns = ['SeniorCitizen', 'Dependents', 'tenure', 'PhoneService', 'PaperlessBilling', 'MonthlyCharges', 'TotalCharges', 'MultipleLines_No_phone_service', 'MultipleLines_Yes', 'InternetService_Fiber_optic', 'InternetService_No', 'OnlineSecurity_No_internet_service', 'OnlineSecurity_Yes', 'OnlineBackup_No_internet_service', 'TechSupport_No_internet_service', 'TechSupport_Yes', 'StreamingTV_No_internet_service', 'StreamingTV_Yes', 'StreamingMovies_No_internet_service', 'StreamingMovies_Yes', 'Contract_One_year', 'Contract_Two_year', 'PaymentMethod_Electronic_check']
+        #Encoding the other categorical categoric features with more than two categories
+        df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
+    elif (option == "Batch"):
+        pass
+        df = df[['SeniorCitizen','Dependents','tenure','PhoneService','MultipleLines','InternetService','OnlineSecurity',
+                'OnlineBackup','TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling','PaymentMethod',
+                'MonthlyCharges','TotalCharges']]
+        df = pd.get_dummies(df)
+        df.drop(columns=cols_to_drop, inplace=True)
+    else:
+        print("Incorrect operational options")
+
+
+    #feature scaling
+    sc = MinMaxScaler()
+    df['tenure'] = sc.fit_transform(df[['tenure']])
+    df['MonthlyCharges'] = sc.fit_transform(df[['MonthlyCharges']])
+    df['TotalCharges'] = sc.fit_transform(df[['TotalCharges']])
+    return df
+        
+
+
+
+
