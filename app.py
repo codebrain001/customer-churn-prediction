8887c7225f25dd2b1dae627ae779294276196385
Brain Aboze <abozejohn@gmail.com>
2021-07-29 04:07:25 +0100

    Completed online prediction

--- app.py
+++ app.py
@@ -30,32 +30,32 @@ def main():
     st.sidebar.image(image)
 
     if add_selectbox == "Online":
-      st.info("Input data below")
-      #Based on our optimal features selection
-      st.subheader("Demographic data")
-      seniorcitizen = st.selectbox('Senior Citizen:', ('Yes', 'No'))
-      dependents = st.selectbox('Dependent:', ('Yes', 'No'))
+        st.info("Input data below")
+        #Based on our optimal features selection
+        st.subheader("Demographic data")
+        seniorcitizen = st.selectbox('Senior Citizen:', ('Yes', 'No'))
+        dependents = st.selectbox('Dependent:', ('Yes', 'No'))
 
 
-      st.subheader("Payment data")
-      tenure = st.slider('Number of months the customer has stayed with the company', min_value=0, max_value=72, value=0)
-      contract = st.selectbox('Contract', ('Month-to-month', 'One year', 'Two year'))
-      paperlessbilling = st.selectbox('Paperless Billing', ('Yes', 'No'))
-      PaymentMethod = st.selectbox('PaymentMethod',('Electronic check', 'Mailed check', 'Bank transfer (automatic)','Credit card (automatic)'))
-      monthlycharges = st.number_input('The amount charged to the customer monthly', min_value=0, max_value=150, value=0)
-      totalcharges = st.number_input('The total amount charged to the customer',min_value=0, max_value=10000, value=0)
+        st.subheader("Payment data")
+        tenure = st.slider('Number of months the customer has stayed with the company', min_value=0, max_value=72, value=0)
+        contract = st.selectbox('Contract', ('Month-to-month', 'One year', 'Two year'))
+        paperlessbilling = st.selectbox('Paperless Billing', ('Yes', 'No'))
+        PaymentMethod = st.selectbox('PaymentMethod',('Electronic check', 'Mailed check', 'Bank transfer (automatic)','Credit card (automatic)'))
+        monthlycharges = st.number_input('The amount charged to the customer monthly', min_value=0, max_value=150, value=0)
+        totalcharges = st.number_input('The total amount charged to the customer',min_value=0, max_value=10000, value=0)
 
-      st.subheader("Services signed up for")
-      mutliplelines = st.selectbox("Does the customer have multiple lines",('Yes','No','No phone service'))
-      phoneservice = st.selectbox('Phone Service:', ('Yes', 'No'))
-      internetservice = st.selectbox("Does the customer have internet service", ('DSL', 'Fiber optic', 'No'))
-      onlinesecurity = st.selectbox("Does the customer have online security",('Yes','No','No internet service'))
-      onlinebackup = st.selectbox("Does the customer have online backup",('Yes','No','No internet service'))
-      techsupport = st.selectbox("Does the customer have technology support", ('Yes','No','No internet service'))
-      streamingtv = st.selectbox("Does the customer stream TV", ('Yes','No','No internet service'))
-      streamingmovies = st.selectbox("Does the customer stream movies", ('Yes','No','No internet service'))
-      
-      data = {
+        st.subheader("Services signed up for")
+        mutliplelines = st.selectbox("Does the customer have multiple lines",('Yes','No','No phone service'))
+        phoneservice = st.selectbox('Phone Service:', ('Yes', 'No'))
+        internetservice = st.selectbox("Does the customer have internet service", ('DSL', 'Fiber optic', 'No'))
+        onlinesecurity = st.selectbox("Does the customer have online security",('Yes','No','No internet service'))
+        onlinebackup = st.selectbox("Does the customer have online backup",('Yes','No','No internet service'))
+        techsupport = st.selectbox("Does the customer have technology support", ('Yes','No','No internet service'))
+        streamingtv = st.selectbox("Does the customer stream TV", ('Yes','No','No internet service'))
+        streamingmovies = st.selectbox("Does the customer stream movies", ('Yes','No','No internet service'))
+
+        data = {
                 'SeniorCitizen': seniorcitizen,
                 'Dependents': dependents,
                 'tenure':tenure,
@@ -73,22 +73,57 @@ def main():
                 'MonthlyCharges': monthlycharges, 
                 'TotalCharges': totalcharges
                 }
-    features_df = pd.DataFrame.from_dict([data])
-    st.markdown("<h3></h3>", unsafe_allow_html=True)
-    st.write('Overview of input is shown below')
-    st.markdown("<h3></h3>", unsafe_allow_html=True)
-    st.dataframe(features_df)
+        features_df = pd.DataFrame.from_dict([data])
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+        st.write('Overview of input is shown below')
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+        st.dataframe(features_df)
 
 
-    #Preprocess inputs
-    preprocess_df = preprocess(features_df, 'Online')
-    st.dataframe(preprocess_df)
-     
+        #Preprocess inputs
+        preprocess_df = preprocess(features_df, 'Online')
+
+        prediction = model.predict(preprocess_df)
+        st.write(prediction)
+
+        if st.button('Predict'):
+            if prediction == 1:
+                st.warning('Yes, the customer will terminate the service.')
+            else:
+                st.success('No, the customer is happy with Telco Services.')
         
 
+    else:
+        if add_selectbox == "Batch":
+            st.subheader("Dataset upload")
+            file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+            if file_upload is not None:
+                st.write(type(file_upload))
+                file_detail = {"filename": file_upload.name,
+                "filetype": file_upload.type,
+                "filesize": file_upload.size}
+                st.write(file_detail)
+            
+            data_df = pd.read_csv(file_upload)
+
+            #Preprocess inputs
+            preprocess_df = preprocess(data_df, "Batch")
+    
+            prediction = model.predict(preprocess_df)
+            st.write(prediction)
+
+            if st.button('Predict'):
+                if prediction == 1:
+                    st.warning('Yes, the customer will terminate the service.')
+                else:
+                    st.success('No, the customer is happy with Telco Services.')
 
 
-    pass
+        
+        
+
+        
+
 
 
 
